def36140fe10f1719ceb71997230a9ff4ae861b4
GustavoCortezC24 <gustavo.cortez@tecsup.edu.pe>
2024-03-25 22:51:21 -0500

    Admin-colab-7

--- src/main/java/com/tecsup/lab3/controller/StudentController.java
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -2,10 +2,9 @@ package com.tecsup.lab3.controller;
 
 public class StudentController {
 
-	public static void main( String[] args )
-    {
-		System.out.println( "This is a StudenController" );
-   }
+	public void nuevaFuncion() {
+        System.out.println("Esta es una nueva función agregada por otro usuario.");
+    }
 }
     
 
