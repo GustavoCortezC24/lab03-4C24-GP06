9f54cbf2a63ff395ad38c2b1561e008a0b8fd631
GustavoCortezC24 <gustavo.cortez@tecsup.edu.pe>
2024-03-25 22:32:04 -0500

    Colab-commit-2

--- src/main/java/com/tecsup/lab3/controller/StudentController.java
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -2,4 +2,10 @@ package com.tecsup.lab3.controller;
 
 public class StudentController {
 
+	public static void main( String[] args )
+    {
+        System.out.println( "This is a StudenController" );
+        System.out.println( "This is a Second Message" );
+        
+    }
 }
